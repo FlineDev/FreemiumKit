de43af18c2645d9d3421524928851ae1870cd807
Cihat Gündüz <commits@cihatguenduez.de>
2023-05-22 13:56:59 +0200

    Change resource specification to specific file

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
       dependencies: [
          .product(name: "IdentifiedCollections", package: "swift-identified-collections")
       ],
-      resources: [.process("Resources")]
+      resources: [.process("Resources/Localizable")]
     )]
 )
