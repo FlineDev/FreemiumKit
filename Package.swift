36fec9a792b34d8fee978faede6e060eee2d7de9
Cihat Gündüz <commits@cihatguenduez.de>
2024-05-25 02:22:25 +0200

    Fix minimum supported tvOS version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "FreemiumKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v16), .visionOS(.v1)],
+    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .visionOS(.v1)],
     products: [.library(name: "FreemiumKit", targets: ["FreemiumKit"])],
     targets: [
       .target(name: "FreemiumKit", dependencies: ["FreemiumKitSDK"]),
